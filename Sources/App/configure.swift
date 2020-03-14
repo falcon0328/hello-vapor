618c0dd5f8538a88c13eba967855670e3b951ee0
aseo <aseo@yahoo-corp.jp>
2020-03-14 22:10:22 +0900

    test postgresql on heroku

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,4 +1,3 @@
-import FluentSQLite
 import FluentPostgreSQL
 import Vapor
 import Leaf
@@ -6,7 +5,7 @@ import Leaf
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     // Register providers first
-    try services.register(FluentSQLiteProvider())
+    try services.register(FluentPostgreSQLProvider())
 
     // Register routes to the router
     let router = EngineRouter.default()
